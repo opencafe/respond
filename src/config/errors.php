51de614e64cba71859c5fa212248041a1dcfe3b0
mehdi hosseini <mehdi@mehdis-MacBook-Pro.local>
2016-08-24 22:46:49 +0430

    update errors file

--- src/config/errors.php
+++ src/config/errors.php
@@ -2,12 +2,124 @@
 
 return [
 
-  'field-not-found' => [ 'Requested field not found', 'error', 1001 ],
+  1001 => [
+    'message' => 'Oops... Requested field is not found!',
+    'type' => 'error'
+  ],
 
-  'warning-message' => [ 'message', 'warning', '#error_code' ],
+  1002 => [
+    'message' => 'Oops... Requested User does not exists!',
+    'type' => 'error'
+  ],
 
-  'error-message' => [ 'message', 'error', '#error_code' ],
+  1003 => [
+    'message' => 'Oops... Client type is not entered!',
+    'type' => 'error'
+  ],
 
-  'info-message' => [ 'message', 'info', '#error_code' ]
+  1004 => [
+    'message' => 'Failed because of duplicate',
+    'type' => 'error'
+  ],
+
+  1005 => [
+    'message' => 'Failed because of dablicated user role',
+    'type' => 'error'
+  ],
+
+  3001 => [
+    'message' => 'You are not logged on',
+    'type' => 'error'
+  ],
+
+  3002 => [
+    'message' => 'Application token did not generated successfully',
+    'type' => 'error'
+  ],
+
+  3003 =>[
+    'message' => 'User token did not generated successfully',
+    'type' => 'error'
+  ],
+
+  3004 => [
+    'message' => 'Request token have no agency access',
+    'type' => 'error'
+  ],
+
+  3005 => [
+    'message' => 'Request token did not contains user information',
+    'type' => 'error'
+  ],
+
+  3006 => [
+    'message' => 'Did not set request token',
+    'type' => 'error'
+  ],
+
+  3007 => [
+    'message' => 'can not decode the token',
+    'type' => 'error'
+  ],
+
+  3008 => [
+    'message' => 'can not generate token for authentication',
+    'type' => 'error'
+  ],
+
+  3009 => [
+    'message' => 'can not create token',
+    'type' => 'error'
+  ],
+
+  5401 => [
+    'message' => 'Authentication unauthorized...',
+    'type' => 'error'
+  ],
+
+  5404 => [
+    'message' => 'Oops... The requested page not found!',
+    'type' => 'error'
+  ],
+
+  5405 => [
+    'message' => 'Oops... The method you requested is not allowed!',
+    'type' => 'error'
+  ],
+
+  5406 => [
+    'message' => 'Oops... The parameters you entered are wrong!',
+    'type' => 'error'
+  ],
+
+  5420 => [
+    'message' => 'Validation Error',
+    'type' => 'error'
+  ],
+
+  5422 => [
+    'message' => 'Token is not valid',
+    'type' => 'error'
+  ],
+
+  5445 => [
+    'message' => 'Oops... Database connection refused',
+    'type' => 'error'
+  ],
+
+  5448 => [
+    'message' => 'Oops... Insert action was not successfully executed',
+    'type' => 'error'
+  ],
+
+  5447 => [
+    'message' => 'Oops... Delete action was not successfully executed',
+    'type' => 'error'
+  ],
+
+  5449 => [
+    'message' => 'Oops... Update action was not successfully executed',
+    'type' => 'error'
+  ]
 
 ];
